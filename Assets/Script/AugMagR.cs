5c41d3bd0e7b1e63dd0b2baee916e4e1051cbbb6
宮本和音 <ohs30427@ohs.hal.ac.jp>
2025-04-05 23:50:01 +0900

    オブジェクトに磁力を持たせる準備
    ・ObjMagnetismスクリプトの作成、アタッチ
    ・のちのち2つに割れるキューブが要るのでElectMagをElectMag_S（SphereのS）に改名
    ・AugmentMagをAugMagLとAugMagRに分割、ZLキーとZRキーの取得を可能にした

---
+++ Assets/Script/AugMagR.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AugMagR : MonoBehaviour
+{
+	[Header("PlayerRの磁石を設定")]
+	public Magnetism magnet;
+
+	private GameInputs inputs;		// GameInputsクラス
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		inputs = new GameInputs();
+		inputs.Enable();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		// ボタンを押されてる強さの取得
+		float RValue = inputs.PlayerR.AugmentMag.ReadValue<float>();
+
+		// 押されてる強さで磁力強化に入るかどうか分岐
+		if (RValue > 0.5f)
+		{
+			AugmentPlayerRMagnetism();
+		}
+
+	}
+
+	private void AugmentPlayerRMagnetism()
+	{
+		//Debug.Log("ZRキー押されてまーす");
+	}
+}
