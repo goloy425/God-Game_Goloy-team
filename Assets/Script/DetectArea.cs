a3fe02409da257438ebef9b396d3f987955b35c8
Hiroto0202 <hnd.hrt.02.02@gmail.com>
2025-04-07 21:50:17 +0900

    複数個磁力を持ったオブジェクトがある場合に対応できるようにした

--- Assets/Script/DetectArea.cs
+++ Assets/Script/DetectArea.cs
@@ -29,6 +29,7 @@ public class DetectArea : MonoBehaviour
 
     private AudioSource audioSource;
     private bool isConnectFg = false; // 回路が繋がったかどうか
+    private int objNum = 0;           // 判定エリア内にいるオブジェクトの数
 
     // Start is called before the first frame update
     void Start()
@@ -65,25 +66,31 @@ public class DetectArea : MonoBehaviour
                 // 判定エリア内のオブジェクトが指定された磁力オブジェクトと同じ時、接続
                 if (other.gameObject == detectedObjects[i])
                 {
-                    // 判定エリアと繋がっている回路の色を変更
-                    for (int j = 0; j < circuitsRenderer.Length; j++)
+                    // 判定エリア内にオブジェクトが存在しない時に実行
+                    if (objNum < 1)
                     {
-                        circuitsRenderer[j].material.color = circuitColor;
+                        // 判定エリアと繋がっている回路の色を変更
+                        for (int j = 0; j < circuitsRenderer.Length; j++)
+                        {
+                            circuitsRenderer[j].material.color = circuitColor;
+                        }
+                        audioSource.PlayOneShot(audioClip);  // SE再生
+                        isConnectFg = true;
+                        OnDetectAreaChanged?.Invoke(true);   // 判定を通知
+                        Debug.Log("接続 isConnectFg:" + isConnectFg);
                     }
-                    audioSource.PlayOneShot(audioClip);     // SE再生
-                    isConnectFg = true;
-                    OnDetectAreaChanged?.Invoke(true);   // 判定を通知
-                    Debug.Log("接続 isConnectFg:" + isConnectFg);
+                    objNum++;
                 }
             }
+
         }
     }
 
     // オブジェクトがすり抜けた時の処理
     private void OnTriggerExit(Collider other)
     {
-        Vector3 thisPos = this.transform.position;      // 判定エリアの座標
-        Vector3 otherPos = other.transform.position;    // 衝突した磁力オブジェクトの座標
+            Vector3 thisPos = this.transform.position;      // 判定エリアの座標
+            Vector3 otherPos = other.transform.position;    // 衝突した磁力オブジェクトの座標
 
         // 指定された全磁力オブジェクトを検索
         for (int i = 0; i < detectedObjects.Length; i++)
@@ -91,15 +98,20 @@ public class DetectArea : MonoBehaviour
             // 判定エリア外のオブジェクトが指定された磁力オブジェクトと同じ時、切断
             if (other.gameObject == detectedObjects[i])
             {
-                // 判定エリアと繋がっている回路の色を変更
-                for (int j = 0; j < circuitsRenderer.Length; j++)
+                // 判定エリア内に一個以下存在する時に実行
+                if (objNum <= 1)
                 {
-                    circuitsRenderer[j].material.color = initCircuitColor;
+                    // 判定エリアと繋がっている回路の色を変更
+                    for (int j = 0; j < circuitsRenderer.Length; j++)
+                    {
+                        circuitsRenderer[j].material.color = initCircuitColor;
+                    }
+                    // audioSource.PlayOneShot(audioClip);     // SE再生
+                    isConnectFg = false;
+                    OnDetectAreaChanged?.Invoke(false);   // 判定を通知
+                    Debug.Log("切断 isConnectFg:" + isConnectFg);
                 }
-                // audioSource.PlayOneShot(audioClip);     // SE再生
-                isConnectFg = false;
-                OnDetectAreaChanged?.Invoke(false);   // 判定を通知
-                Debug.Log("切断 isConnectFg:" + isConnectFg);
+                objNum--;
             }
         }
     }
