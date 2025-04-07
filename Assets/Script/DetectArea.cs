23ddd65edb382d253b27807cb1342ec9f3e33a56
Hiroto0202 <hnd.hrt.02.02@gmail.com>
2025-04-07 19:52:50 +0900

    回路がつながった時にSEが再生されるようにした

--- Assets/Script/DetectArea.cs
+++ Assets/Script/DetectArea.cs
@@ -15,6 +15,8 @@ public class DetectArea : MonoBehaviour
     public float sizeX = 0.0f;
     [Header("判定を取る磁力オブジェクト")]
     public GameObject[] detectedObjects;
+    [Header("接続前の回路の色")]
+    public Color initCircuitColor;
     [Header("接続後の回路の色")]
     public Color circuitColor;
     [Header("判定エリアと繋がっている回路")]
@@ -31,12 +33,17 @@ public class DetectArea : MonoBehaviour
         audioSource = GetComponent<AudioSource>();
         // 色を変更
         GetComponent<Renderer>().material.color = color;
+        // 回路の色を初期化
+        for (int i = 0; i < circuitsRenderer.Length; i++)
+        {
+            circuitsRenderer[i].material.color = initCircuitColor;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     // オブジェクトが衝突した時の処理
@@ -47,23 +54,47 @@ public class DetectArea : MonoBehaviour
 
         // 衝突した磁力オブジェクトが判定エリア内にいる時
         if (thisPos.x - sizeX <= otherPos.x && otherPos.x <= thisPos.x + sizeX &&
-           thisPos.z - sizeZ <= otherPos.z && otherPos.z <= thisPos.z + sizeZ)
+            thisPos.z - sizeZ <= otherPos.z && otherPos.z <= thisPos.z + sizeZ)
         {
             // 指定された全磁力オブジェクトを検索
             for (int i = 0; i < detectedObjects.Length; i++)
             {
-                // 判定エリア内のオブジェクトが指定された磁力オブジェクトと同じ時、繋げる
+                // 判定エリア内のオブジェクトが指定された磁力オブジェクトと同じ時、接続
                 if (other.gameObject == detectedObjects[i])
                 {
                     // 判定エリアと繋がっている回路の色を変更
                     for (int j = 0; j < circuitsRenderer.Length; j++)
                     {
                         circuitsRenderer[j].material.color = circuitColor;
-                        audioSource.PlayOneShot(audioClip);     // SE再生
                     }
                     isConnectFg = true;
-                    Debug.Log(isConnectFg);
+                    audioSource.PlayOneShot(audioClip);     // SE再生
+                    Debug.Log("接続" + isConnectFg);
                 }
+            }
+        }
+    }
+
+    // オブジェクトがすり抜けた時の処理
+    private void OnTriggerExit(Collider other)
+    {
+        Vector3 thisPos = this.transform.position;      // 判定エリアの座標
+        Vector3 otherPos = other.transform.position;    // 衝突した磁力オブジェクトの座標
+
+        // 指定された全磁力オブジェクトを検索
+        for (int i = 0; i < detectedObjects.Length; i++)
+        {
+            // 判定エリア外のオブジェクトが指定された磁力オブジェクトと同じ時、切断
+            if (other.gameObject == detectedObjects[i])
+            {
+                // 判定エリアと繋がっている回路の色を変更
+                for (int j = 0; j < circuitsRenderer.Length; j++)
+                {
+                    circuitsRenderer[j].material.color = initCircuitColor;
+                }
+                isConnectFg = false;
+                // audioSource.PlayOneShot(audioClip);     // SE再生
+                Debug.Log("切断" + isConnectFg);
             }
         }
     }
