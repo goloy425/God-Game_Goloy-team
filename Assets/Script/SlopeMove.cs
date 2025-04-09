0e1e0a683d10fac43ddce7d04e1d06df85a2dda4
zinzin129 <hatohiji8@gmail.com>
2025-04-09 22:06:06 +0900

    坂の上り下りできるようになった（バグあり）

---
+++ Assets/Script/SlopeMove.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlopeMove : MonoBehaviour
+{
+    public float moveSlopeSpeed = 3.0f;
+    public float maxSlopeAngle = 45.0f;
+    public float gravityMultiplier = 100.0f;
+
+    bool colSlope = false;
+
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        // キャラクターの移動方向を取得
+        Vector3 moveDirection = rb.velocity.normalized;
+
+        //if (colSlope)
+        {
+            MoveOnSlope(moveDirection);
+        }
+    }
+
+    void MoveOnSlope(Vector3 moveDirection)
+    {
+        RaycastHit hit;
+        // キャラクターの足元にRayを飛ばす
+        if (Physics.Raycast(transform.position - new Vector3(0, 1, 0), Vector3.down, out hit, 1f))
+        {
+            Debug.DrawRay(transform.position - new Vector3(0, 1, 0), Vector3.down * 10, Color.red);
+            // 斜面の法線を取得
+            Vector3 normal = hit.normal;
+
+            //Vector3 pos = transform.position;
+            //transform.position.Set(pos.x, pos.y+1, pos.z);
+
+            // 斜面の角度を計算
+            float slopeAngle = Vector3.Angle(Vector3.up, normal);
+
+            if(slopeAngle<=0.1f)
+            {
+                return;
+            }
+
+            // 移動方向を斜面に沿わせる
+            Vector3 slopeDirection = Vector3.ProjectOnPlane(moveDirection, normal);
+            Debug.Log(slopeDirection);
+            
+            transform.position += (slopeDirection * moveSlopeSpeed * Time.deltaTime);
+
+            //rb.MovePosition(transform.position + slopeDirection * moveSlopeSpeed /** Time.deltaTime*/);
+
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Slope"))
+            colSlope = true;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Slope"))
+            colSlope = false;
+    }
+}
+
