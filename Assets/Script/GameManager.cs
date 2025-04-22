82eeef9f1513733545c50c0c222c97343a2c5feb
Hiroto0202 <hnd.hrt.02.02@gmail.com>
2025-04-22 22:59:12 +0900

    ゲームマネージャーに各ステージの磁力オブジェクトと判定エリアを設定するように変更
    これによってゲームが強制終了することは
    なくなったが、磁力オブジェクトが複数個
    存在するステージでは強制終了するかも。
    磁力スクリプトを少し変更する方が早い？
    かもしれないので進行が滞った場合は検討。

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -3,19 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-// 制作者　ゴロイヒデキ
+// 制作者　ゴロイヒデキ   本田洸都
+
+// 外部から設定可能なステージのデータ
+[System.Serializable]
+public class StageData
+{
+    public List<GameObject> magObjects;     // 磁力オブジェクト
+    public List<DetectArea> detectAreas;    // クリア判定オブジェクト
+    public bool clearFg = false;            // クリアフラグ
+}
 
 public class GameManager : MonoBehaviour
 {
+    [Header("各ステージの磁力オブジェクト（最初にステージ数を設定）")]
+    public List<StageData> stageDatas;
+
     [Header("プレイヤーの磁石")]
     public GameObject magnet1;
     public GameObject magnet2;
 
     public DetectArea[] detectAreas;    // すべての判定エリアを登録
-    private int totalConnected = 0;       // 接続された判定エリアの数
-
-    public PressurePlates01[] pressurePlates; // すべての感圧板を登録
-    private int totalPressed = 0; // 押されている感圧板の数
+    private int totalConnected = 0;     // 接続された判定エリアの数
 
     public string resultSceneName = "Result";       // 遷移先のシーン名をInspectorで設定
     public string gameOverSceneName = "GameOver";   // 遷移先のシーン名をInspectorで設定
@@ -27,6 +36,12 @@ public class GameManager : MonoBehaviour
 
     private Magnetism magnetism1 = null;      // プレイヤーLのマグネティズム
     private Magnetism magnetism2 = null;      // プレイヤーRのマグネティズム
+
+    private int curStage = 0;                 // 現在のステージ数
+
+    public PressurePlates01[] pressurePlates; // すべての感圧板を登録
+    private int totalPressed = 0; // 押されている感圧板の数
+
 
     // Start is called before the first frame update
     void Start()
@@ -44,6 +59,9 @@ public class GameManager : MonoBehaviour
         // 磁力スクリプトを取得
         magnetism1 = magnet1.GetComponent<Magnetism>();
         magnetism2 = magnet2.GetComponent<Magnetism>();
+
+        // ステージ1の磁力オブジェクトを有効化
+        ActiveMagObjects(0);
     }
 
     // Update is called once per frame
@@ -54,8 +72,34 @@ public class GameManager : MonoBehaviour
 
     private void FixedUpdate()
     {
-        // プレイヤーの磁石に何かがくっついた時
-        if((magnetism1.isSnapping || magnetism2.isSnapping) || 
+        int connectCount = 0;  // 繋がっている判定エリア
+
+        // 現在のステージの判定エリアが繋がっているかを調べる
+        for (int i = 0; i < stageDatas[curStage].detectAreas.Count; i++)
+        {
+            // 繋がっている時、その数をカウント
+            if (stageDatas[curStage].detectAreas[i].GetIsConnectFg())
+            {
+                connectCount++;
+
+                // すべての判定エリアが繋がっている時、クリアフラグを立てる
+                if (connectCount == stageDatas[curStage].detectAreas.Count)
+                {
+                    stageDatas[curStage].clearFg = true;
+                }
+            }
+        }
+
+        // 現在のステージをクリアした時、次のステージに進む
+        if (stageDatas[curStage].clearFg )
+        {
+            curStage++;
+            ActiveMagObjects(curStage); // 次のステージの磁力オブジェクトを有効化
+            stageDatas[curStage - 1].clearFg = false;   // 複数回更新しないようにフラグを折る
+        }
+
+        // プレイヤーの磁石に何かがくっついた時または、磁力範囲外に出た時
+        if ((magnetism1.isSnapping || magnetism2.isSnapping) || 
             (!magnetism1.inMagnetismArea || !magnetism2.inMagnetismArea))
         {
             Debug.Log("磁石がくっつきました！ゲームオーバー！Result画面に移ります");
@@ -96,6 +140,29 @@ public class GameManager : MonoBehaviour
             Invoke("MoveResultScene", changeSceneTime);
         }
     }
+
+    // 指定したステージの磁力オブジェクトを有効化
+    private void ActiveMagObjects(int _stageIndex)
+    {
+        // 全ステージの磁力オブジェクトを無効化
+        for (int i = 0; i < stageDatas.Count; i++)
+        {
+            foreach (GameObject obj in stageDatas[i].magObjects)
+            {
+                if (obj != null) { obj.SetActive(false); }
+            }
+        }
+
+        // 指定されたステージのオブジェクトを有効化
+        if(_stageIndex >= 0 &&  _stageIndex < stageDatas.Count)
+        {
+            foreach (GameObject obj in stageDatas[_stageIndex].magObjects)
+            {
+                if (obj != null) { obj.SetActive(true); }
+            }
+        }
+    }
+
 
     // リザルトシーンに遷移
     private void MoveResultScene()
