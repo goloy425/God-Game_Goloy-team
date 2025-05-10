e89f0a5767b1befcb227ac641ef09bf29c0d3d0d
Hiroto0202 <hnd.hrt.02.02@gmail.com>
2025-05-10 12:14:11 +0900

    ドアのライトの色が変わらなくなっていたので修正

--- Assets/Script/Stage/Object/OpenDoor.cs
+++ Assets/Script/Stage/Object/OpenDoor.cs
@@ -3,91 +3,108 @@
 // クリア時のドアが開く処理
 // doorFlameオブジェクトにアタッチする
 //--------------------------------------------------------
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class OpenDoor : MonoBehaviour
 {
-	[Header("ドアが開く速度")]
-	public float moveSpeed = 1.8f;
+    [Header("ドアが開く速度")]
+    public float moveSpeed = 1.8f;
 
-	[Header("ドアが開くのにかかる秒数")]
-	public float openTime = 1.0f;
+    [Header("ドアが開くのにかかる秒数")]
+    public float openTime = 1.0f;
 
-	[Header("ドアがあるステージ数")]
-	public int curStage;
+    [Header("ドアがあるステージ数")]
+    public int curStage;
 
-	private GameManager gameManager;    // ゲームマネージャー
-	private GameObject leftDoor;
-	private GameObject rightDoor;
-	private bool openFg = false;            // 開けるかどうかのフラグ
-	private bool completeOpenFg = false;    // 開いたかどうかのフラグ
-	private float timer = 0.0f;
+    private GameManager gameManager;    // ゲームマネージャー
+    private GameObject leftDoor;
+    private GameObject rightDoor;
+    private bool openFg = false;            // 開けるかどうかのフラグ
+    private bool completeOpenFg = false;    // 開いたかどうかのフラグ
+    private float timer = 0.0f;
 
-	private bool isMoving = false;  // ドアが動いているかどうか（Vibrationスクリプトにて使用）
+    Material mat;		// 使用しているマテリアル
+    private Color emissionColor = new Color(0.24f, 0.75f, 0.39f, 1.0f);		// 変更後の色
+    private float intensity = 3.5f;		// 光の強度
+    private Color finalEmissionColor;   // 色と光の強度から決定された色
 
-	// Start is called before the first frame update
-	void Start()
-	{
-		// ゲームマネージャーを取得
-		gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+    private bool isMoving = false;  // ドアが動いているかどうか（Vibrationスクリプトにて使用）
 
-		// 左右のドアオブジェクトを取得
-		leftDoor = transform.Find("doorL_rend").gameObject;
-		rightDoor = transform.Find("doorR_rend").gameObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // ゲームマネージャーを取得
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
 
-		openFg = false;
-	}
+        // 左右のドアオブジェクトを取得
+        leftDoor = transform.Find("doorL_rend").gameObject;
+        rightDoor = transform.Find("doorR_rend").gameObject;
 
-	// Update is called once per frame
-	void Update()
-	{
-		
-	}
+        openFg = false;
 
-	private void FixedUpdate()
-	{
-		//// テストでキーボードのAを押すと動く
-		//if (Input.GetKey(KeyCode.A))
-		//{
-		//    openFg = true;
-		//    Debug.Log(openFg);
-		//}
+        //------ ドアが開いた後のライトの色 ------//
+        // マテリアルを取得
+        mat = transform.Find("doorframe_render").GetComponent<Renderer>().material;
+        // HDRの強度を適用（Color * 強度）
+        finalEmissionColor = emissionColor * intensity;
+    }
 
-		// 添え字で指定するので-1する
-		openFg = gameManager.GetStageClearFg(curStage - 1);
+    // Update is called once per frame
+    void Update()
+    {
 
-		// フラグが立っているかつ、開けるのにかかる秒数以内の時
-		if (openFg && !completeOpenFg && timer <= openTime)
-		{
-			timer += Time.deltaTime;
-			// 左のドアを移動
-			leftDoor.transform.Translate(Vector3.right * moveSpeed * Time.deltaTime, Space.Self);
-			// 右のドアを移動
-			rightDoor.transform.Translate(Vector3.left * moveSpeed * Time.deltaTime, Space.Self);
+    }
 
-			isMoving = true;
-		}
-		else if(timer > openTime)
-		{
-			completeOpenFg = true;
-			timer = 0.0f;
+    private void FixedUpdate()
+    {
+        //// テストでキーボードのAを押すと動く
+        //if (Input.GetKey(KeyCode.A))
+        //{
+        //    openFg = true;
+        //    Debug.Log(openFg);
+        //}
 
-			isMoving = false;
-		}
-	}
+        // 添え字で指定するので-1する
+        openFg = gameManager.GetStageClearFg(curStage - 1);
 
-	// 開いたかどうかのゲッター
-	public bool GetCompleteOpenFg()
-	{
-		return completeOpenFg;
-	}
+        // フラグが立っているかつ、開けるのにかかる秒数以内の時
+        if (openFg && !completeOpenFg && timer <= openTime)
+        {
+            // Emissionを有効化
+            mat.SetColor("_EmissionColor", finalEmissionColor);
+            // Emissionを適用するためのフラグを設定
+            mat.EnableKeyword("_EMISSION");
+
+            timer += Time.deltaTime;
+            // 左のドアを移動
+            leftDoor.transform.Translate(Vector3.right * moveSpeed * Time.deltaTime, Space.Self);
+            // 右のドアを移動
+            rightDoor.transform.Translate(Vector3.left * moveSpeed * Time.deltaTime, Space.Self);
+
+            isMoving = true;
+        }
+        else if (timer > openTime)
+        {
+            completeOpenFg = true;
+            timer = 0.0f;
+
+            isMoving = false;
+        }
+    }
+
+    // 開いたかどうかのゲッター
+    public bool GetCompleteOpenFg()
+    {
+        return completeOpenFg;
+    }
 
 
-	// ドアが動いているかどうかのゲッター
-	public bool GetDoorMovingFg()
-	{
-		return isMoving;
-	}
+    // ドアが動いているかどうかのゲッター
+    public bool GetDoorMovingFg()
+    {
+        return isMoving;
+    }
 }
