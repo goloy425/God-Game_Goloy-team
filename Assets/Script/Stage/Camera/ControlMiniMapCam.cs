2d03ac75a8c10bc64774ea750922177548e31da5
Okazunzun <146701765+Okazunzun@users.noreply.github.com>
2025-05-14 23:53:13 +0900

    ミニマップ（仮）の作成+GameInputs追加
    開始ステージを切り替えればマップも変わる…ゲーム中にマップが変わるようにするのとプレイヤーのアイコン表示とかの見やすさアップは明日やる
    あと相変わらずPS5のコントローラーで強化しようとするとポーズが開くのでついでにGameInputsにポーズ用のキー入力を追加した、まだポーズ開くスクリプトは弄ってない

---
+++ Assets/Script/Stage/Camera/ControlMiniMapCam.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//==================================================================
+// 作成者：宮本和音
+// ミニマップ用のカメラのオンオフを切り替える
+//==================================================================
+
+public class ControlMiniMapCam : MonoBehaviour
+{
+	[Header("miniMapCamをステージ順に設定")]
+	public Camera[] miniMapCams;
+
+	[Header("GameManagerを設定")]
+	public GameManager gm;
+
+	private int curStage;
+	private int prevStage;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		// 開始時のカメラを設定
+		switch (gm.GetStartStage() - 1)
+		{
+			case 0:
+				miniMapCams[0].gameObject.SetActive(true);
+				break;
+			case 1:
+				miniMapCams[1].gameObject.SetActive(true);
+				break;
+			case 2:
+				miniMapCams[2].gameObject.SetActive(true);
+				break;
+			case 3:
+				miniMapCams[3].gameObject.SetActive(true);
+				break;
+			case 4:
+				miniMapCams[4].gameObject.SetActive(true);
+				break;
+		}
+
+		// 最初は揃えておく
+		curStage = gm.GetCurStage();
+		prevStage = curStage;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		curStage = gm.GetCurStage() - 1;
+
+		// カメラの切り替え
+		if (curStage != prevStage)
+		{
+			SwitchminiMapCam();
+		}
+
+		prevStage = curStage;
+	}
+
+	//--- ミニマップカメラの切り替え ---//
+	private void SwitchminiMapCam()
+	{
+		miniMapCams[prevStage - 1].gameObject.SetActive(false);
+		miniMapCams[curStage - 1].gameObject.SetActive(true);
+	}
+}
