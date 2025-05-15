c3d734d97c6a1b6f6af454f86ba83f0c7d330230
Okazunzun <146701765+Okazunzun@users.noreply.github.com>
2025-05-15 23:55:22 +0900

    とりあえずクリアした瞬間に次のステージのマップが表示されるようにした
    さすがにすぐすぎる気がするがワープするならこれでもまあ…？ワープを試してからタイミング調整する

--- Assets/Script/Stage/Object/SwitchMap.cs
+++ Assets/Script/Stage/Object/SwitchMap.cs
@@ -15,24 +15,44 @@ public class SwitchMap : MonoBehaviour
 	[Header("GameManagerを設定")]
 	public GameManager gm;
 
+	private int curStage;
+	private int prevStage;
+
 	// Start is called before the first frame update
 	void Start()
 	{
 		// 邪魔なので作成中以外は非アクティブにしてあるはずだが、一応全部非アクティブにしておく
-		//for (int i = 0; i < Objects.Length; i++)
-		//{
-		//	Objects[i].gameObject.SetActive(false);
-		//}
-
-		Objects[0].gameObject.SetActive(false);		// デバッグ用　まだ1-1しかないため
+		for (int i = 0; i < Objects.Length; i++)
+		{
+			Objects[i].gameObject.SetActive(false);
+		}
 
 		// 開始ステージのマップだけアクティブにする
 		Objects[gm.GetStartStage() - 1].gameObject.SetActive(true);
+
+		// 最初は揃えておく
+		curStage = gm.GetCurStage();
+		prevStage = curStage;
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
-		
+		curStage = gm.GetCurStage();
+
+		// カメラの切り替え
+		if (curStage != prevStage)
+		{
+			SwitchMiniMap();
+		}
+
+		prevStage = curStage;
+	}
+
+	//--- ミニマップの切り替え ---//
+	private void SwitchMiniMap()
+	{
+		Objects[prevStage].gameObject.SetActive(false);
+		Objects[curStage].gameObject.SetActive(true);
 	}
 }
