ef3108cd486e9ea34122b7523edb9ba58ebfab49
goloy425 <goloy30488@gmail.com>
2025-05-18 01:57:23 +0900

    フェード出来るようになった
    タイトルだけ適用中。
    フェードしたいものにCanvas groupをつけないとしっかり動かない。

--- Assets/Script/UI/Fade.cs
+++ Assets/Script/UI/Fade.cs
@@ -6,24 +6,27 @@ using UnityEngine.UI;
 // 制作者　ゴロイ
 public class Fade : MonoBehaviour
 {
+    // フェードするオブジェクト情報を管理するクラス
     [System.Serializable]
     public class FadeObject
     {
         public GameObject targetObj;  // フェードする画像
-        public bool fadeIn = true;       // フェードインするかフェードアウトするか
-        public float fadeTime = 1.0f; // フェードの時間
+        public bool fadeIn = true;    // フェードインするかフェードアウトするか
+        public float fadeTime = 1.0f; // フェードの時間（秒）
+        public float delay = 0.0f;    // フェード開始までの遅延時間（秒）
     }
 
     public List<FadeObject> fadeObjects = new List<FadeObject>(); // Inspectorで複数オブジェクトを設定
 
-    private Dictionary<GameObject, float> startTimes = new Dictionary<GameObject, float>();
+    private Dictionary<GameObject, float> startTimes = new Dictionary<GameObject, float>(); // 各オブジェクトのフェード開始時間を管理するもの
 
     // Start is called before the first frame update
     void Start()
     {
+        // ゲーム開始時に各オブジェクトのフェード開始時間を記録
        foreach(var fadeObj in fadeObjects)
         {
-            startTimes[fadeObj.targetObj] = Time.time; // 各オブジェクトの開始時間を設定
+            startTimes[fadeObj.targetObj] = Time.time + fadeObj.delay; // 各オブジェクトの開始時間 + 遅延時間を設定
         }
     }
 
@@ -34,23 +37,30 @@ public class Fade : MonoBehaviour
         {
             if (fadeObj.targetObj == null) continue; // オブジェクトが設定されていない場合はスキップ
 
-            float elapsedTime = Time.time - startTimes[fadeObj.targetObj];
+            float elapsedTime = Time.time - startTimes[fadeObj.targetObj]; // 経過時間を計算
+            
+            // フェードの進行度を計算
             float alphaValue = fadeObj.fadeIn
-                ? 1.0f - (elapsedTime / fadeObj.fadeTime)
-                : (elapsedTime / fadeObj.fadeTime);
+                ? 1.0f - (elapsedTime / fadeObj.fadeTime) // フェードイン
+                : (elapsedTime / fadeObj.fadeTime);       // フェードアウト
 
             alphaValue = Mathf.Clamp01(alphaValue); // α値を 0-1 に制限
 
-            Image img = fadeObj.targetObj.GetComponent<Image>();
-            if (img != null)
+            // "CanvasGroup"を使用してフェード処理を適用
+            CanvasGroup canvasGroup = fadeObj.targetObj.GetComponent<CanvasGroup>();
+            if(canvasGroup != null)
             {
-                img.color = new Color(0, 0, 0, alphaValue);
+                canvasGroup.alpha = alphaValue;
             }
+
+
+            //// UI要素を取得し、フェードを適応
+            //Image img = fadeObj.targetObj.GetComponent<Image>();
+            //if (img != null)
+            //{
+            //    img.color = new Color(0, 0, 0, alphaValue);
+            //}
         }
 
-    }
-    public void Load()
-    {
-        Application.LoadLevel(Application.loadedLevelName);
     }
 }
