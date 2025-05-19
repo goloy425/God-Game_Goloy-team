ba4f1cdb0973b3487a1a2bf5ea9eeaa301bb459c
Hiroto0202 <hnd.hrt.02.02@gmail.com>
2025-05-19 23:39:36 +0900

    ゲームオーバーのエフェクトを導入したが連続で表示される
    フラグが通り続けているせいで起こっている

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -123,6 +123,10 @@ public class GameManager : MonoBehaviour
     public GameObject magnet1;
     public GameObject magnet2;
 
+    [Header("ゲームオーバー時のプレイヤーのエフェクト")]
+    public GameObject gameOverEffectL;
+    public GameObject gameOverEffectR;
+
     [Header("開始ステージ")]
     public int startStage;
 
@@ -132,6 +136,7 @@ public class GameManager : MonoBehaviour
 
     private bool gameClearFg = false;         // ゲームクリアしたかどうか
     private bool gameOverFg = false;          // ゲームオーバーしたかどうか
+    private bool gameOverEffectFg = false;    // ゲームオーバーのエフェクトをするかどうか
     private bool augMagFg = false;            // 磁力強化できるかどうか
 
     private int totalAreas = 0;               // 設定された判定エリアの数
@@ -364,10 +369,19 @@ public class GameManager : MonoBehaviour
             // フラグからゲームオーバーに遷移
             if (gameOverFg)
             {
+                // ゲームオーバーエフェクトを生成
+                gameOverEffectFg = true;
+
                 // changeSceneTime秒後にゲームオーバーシーンに遷移
                 Invoke("MoveGameOverScene", changeSceneTime);
                 Debug.Log("ゲームオーバー！Result画面に移ります");
             }
+        }
+
+        //------ エフェクト処理 ------//
+        if(gameOverEffectFg)
+        {
+            Invoke("CreateGameOverEffet", 0.0f);
         }
 
         //------ フェード処理 ------//
@@ -795,6 +809,13 @@ public class GameManager : MonoBehaviour
         playerR.transform.rotation = stageData[curStage].playerRRotation;
 
         fadeInFg = true;
+    }
+
+    // ゲームオーバー時のエフェクトをする処理
+    private void CreateGameOverEffet()
+    {
+        Instantiate(gameOverEffectL, playerL.transform.position, Quaternion.identity);  // プレイヤーL
+        Instantiate(gameOverEffectR, playerR.transform.position, Quaternion.identity);  // プレイヤーR
     }
 
     // リザルトシーンに遷移
