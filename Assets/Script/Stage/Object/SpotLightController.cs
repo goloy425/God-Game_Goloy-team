2877c17daa7ede3d4d67b564bf1b4e1a7752f9ab
宮本和音 <ohs30427@ohs.hal.ac.jp>
2025-05-26 00:36:54 +0900

    スポットライト（途中）
    見た目はそれっぽいのができた、今は動きの実装中
    動きの実装迷走しすぎて当たり判定関連はまだ手付かず

---
+++ Assets/Script/Stage/Object/SpotLightController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+//===========================================================
+// 作成者：宮本和音
+// スポットライトの動き（首振り）を制御するスクリプト
+//===========================================================
+
+public class SpotLightController : MonoBehaviour
+{
+	[Header("ターゲットの角度を2つ設定")]
+	public float[] targetAngle;
+
+	[Header("ターゲットに留まる秒数")]
+	public float stayTime = 2.0f;
+
+	private float stayTimer = 0f;
+	public int currentNum = 0;
+
+	private Quaternion rota;
+	private float newRotaX;
+
+    void Update()
+	{
+        rota = transform.rotation;		// 角度の更新
+
+        if (targetAngle == null) return;
+
+		newRotaX += 0.001f;
+		transform.rotation = new Quaternion(newRotaX, rota.y, rota.z, rota.w);
+	}
+}
