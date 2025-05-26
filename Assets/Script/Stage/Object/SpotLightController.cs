97b3853971893e8603ff6765ed13c45dd703464e
宮本和音 <ohs30427@ohs.hal.ac.jp>
2025-05-26 10:52:17 +0900

    動き完成
    当たり判定はこれから

--- Assets/Script/Stage/Object/SpotLightController.cs
+++ Assets/Script/Stage/Object/SpotLightController.cs
@@ -10,25 +10,102 @@ using UnityEngine;
 
 public class SpotLightController : MonoBehaviour
 {
-	[Header("ターゲットの角度を2つ設定")]
-	public float[] targetAngle;
+	[Header("ターゲットの角度を設定(y=0固定！)")]
+	public Vector3[] targetAngle;
+
+	[Header("動く速度")]
+	public float rotateSpeed = 0.1f;
 
 	[Header("ターゲットに留まる秒数")]
 	public float stayTime = 2.0f;
 
-	private float stayTimer = 0f;
-	public int currentNum = 0;
+	private float stayTimer = 0.0f;		// 計測用のタイマー
+	private int currentNum = 0;
 
-	private Quaternion rota;
-	private float newRotaX;
+	private float currentX;
+	private float targetX;
+	private float deltaX;
+	private bool moveX;
 
-    void Update()
+	private float currentZ;
+	private float targetZ;
+	private float deltaZ;
+	private bool moveZ;
+
+	void Update()
 	{
-        rota = transform.rotation;		// 角度の更新
+		if (targetAngle == null) return;
 
-        if (targetAngle == null) return;
+		stayTimer += Time.deltaTime;
 
-		newRotaX += 0.001f;
-		transform.rotation = new Quaternion(newRotaX, rota.y, rota.z, rota.w);
+		if (targetAngle[currentNum].x != 0.0f)
+		{
+			currentX = transform.localEulerAngles.x;
+			targetX = targetAngle[currentNum].x;
+			deltaX = Mathf.DeltaAngle(currentX, targetX);
+			moveX = true;
+		}
+
+		if (targetAngle[currentNum].z != 0.0f)
+		{
+			currentZ = transform.localEulerAngles.z;
+			targetZ = targetAngle[currentNum].z;
+			deltaZ = Mathf.DeltaAngle(currentZ, targetZ);
+			moveZ = true;
+		}
+
+		// 最短の角度差を求める（-180〜180°の範囲で出る）
+
+
+		// 一定のスピードで回転
+		if (moveX && moveZ)
+		{
+			if (Mathf.Abs(deltaX) > 0.5f && Mathf.Abs(deltaZ) > 0.5f)  // ある程度近づいたら止める
+			{
+				float stepX = Mathf.Sign(deltaX) * rotateSpeed;
+				float stepZ = Mathf.Sign(deltaZ) * rotateSpeed;
+				transform.Rotate(stepZ, 0f, stepZ, Space.Self);
+			}
+			else
+			{
+				UpdateTimer();
+			}
+		}
+		else if (moveX)
+		{
+			if (Mathf.Abs(deltaX) > 0.5f)
+			{
+				float stepX = Mathf.Sign(deltaX) * rotateSpeed;
+				transform.Rotate(stepX, 0f, 0f, Space.Self);
+			}
+			else
+			{
+				UpdateTimer();
+			}
+		}
+		else if(moveZ)
+		{
+			if (Mathf.Abs(deltaZ) > 0.5f)
+			{
+				float stepZ = Mathf.Sign(deltaX) * rotateSpeed;
+				transform.Rotate(stepZ, 0f, 0f, Space.Self);
+			}
+			else
+			{
+				UpdateTimer();
+			}
+		}
+	}
+
+	private void UpdateTimer()
+	{
+		// タイマー更新
+		stayTimer += Time.deltaTime;
+
+		if (stayTimer > stayTime)
+		{
+			stayTimer = 0f;
+			currentNum = (currentNum + 1) % targetAngle.Length;
+		}
 	}
 }
