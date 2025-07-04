8fe548fed048796d2f1ded9a3e453e750798036e
Hiroto0202 <hnd.hrt.02.02@gmail.com>
2025-07-04 18:47:34 +0900

    UIをそれぞれの頭上に配置（のちに修正予定）

--- Assets/Script/UI/ModifyPlayerUIScale.cs
+++ Assets/Script/UI/ModifyPlayerUIScale.cs
@@ -12,16 +12,20 @@ public class ModifyPlayerUIScale : MonoBehaviour
     private Vector3 prevCamScale = new Vector3(0.0f, 0.0f, 0.0f);
     private Vector3 curCamPos = new Vector3(0.0f, 0.0f, 0.0f);
 
+    // UIの向きベクトル
+    private Vector3 UIdir = new Vector3(0.0f, 0.0f, 0.0f);
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // カメラの方向に向きベクトルを設定
+        UIdir = -mainCamera.transform.forward;  
+        this.transform.forward = UIdir;
     }
 
     // Update is called once per frame
     void Update()
     {
-        prevCamScale = curCamPos;
-        curCamPos = mainCamera.transform.localScale;
+
     }
 }
